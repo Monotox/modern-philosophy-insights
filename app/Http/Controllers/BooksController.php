be40c09bc86aaec0e5ac6a5523f40bf07930f30e
José Campillo <jose.campillo@hotmail.com>
2022-04-23 22:21:44 -0300

    Initial commit. Modeled migrations.

---
+++ app/Http/Controllers/BooksController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class BooksController extends Controller
+{
+    public function index()
+    {
+        return "ok";
+    }
+}
---
+++ database/migrations/2022_04_24_010619_create_table_thoughts.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('thoughts', function (Blueprint $table) {
+            $table->id();
+            $table->string('lyric');
+            $table->string('youtube_id');
+            $table->integer('time');
+            $table->integer('book_id');
+            $table->foreign('book_id')
+            ->references('books')
+            ->on('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('thoughts');
+    }
+};
